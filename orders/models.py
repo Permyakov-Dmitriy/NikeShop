83de1084d857a6a137209ad6bbb36fa68e72beeb
Permyakov-Dmitriy <dima.perm11@gmail.com>
2023-07-29 17:19:23 +0600

    add bucket model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from auth_nike.models import NikeUser
+
+from shop_nike.models import Product
+
+
+class BucketModel(models.Model):
+    user_id = models.ForeignKey(NikeUser, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id.email} {self.product_id.title_product}'
