ec515369a6ea7f0e6fe8c826744ee088cefb118e
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-07-30 23:09:00 +0600

    comment for shop_nike views

--- shop_nike/views.py
+++ shop_nike/views.py
@@ -11,41 +11,50 @@ from orders.models import Basket
 
 
 class ShopView(LoginRequiredMixin, TemplateView):
+    ''' Страница магазина с продуктами '''
     template_name = 'shop_nike/shop.html'
     login_url = '/auth'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Гендер продуктов
         gender = self.request.GET.get('gender', '')
+        # Категория продуктов
         category = self.request.GET.get('category', '')
 
+        # Фильтрация по гендеру для последующего фильтра по категории
         products = Product.objects.filter(gender=gender)
 
         if category:
             products = products.filter(category=category)
 
         context['gender'] = gender
+        # Случайная сортировка продуктов
         context['products'] = products.order_by('?')
         
         return context
 
 
 class ProductView(LoginRequiredMixin, TemplateView):
+    ''' Страница продукта '''
     template_name = 'shop_nike/product.html'
     login_url = '/auth'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        product_id = self.request.GET.get('id', 1)
+        # Вытаскиваем id продукта если нет то савим 0 для будущего провоцирования ошибки
+        product_id = self.request.GET.get('id', 0)
 
         user = NikeUser.objects.get(id=self.request.user.id)
 
+        # Если продукта с таким id нет то возвращаем страницу 404
         try:
             product = Product.objects.get(id = product_id)
         except ObjectDoesNotExist:
             raise Http404()
         
+        # Берем продукт из карзины для установки лимита на добавления
         bucket = Basket.objects.filter(product_id = product_id)
         limit = len(bucket) == 9
         
@@ -64,6 +73,7 @@ class ProductView(LoginRequiredMixin, TemplateView):
         # Выбираем только те продукты которые есть в массиве и только первые три 
         recomend_list_products = Product.objects.filter(id__in=product_ids)[:3]
 
+        # Установка id избранного и проверки на избранное
         try:
             favorite = FavoriteModel.objects.get(product_id=product, user_id=user)
             fav_id = favorite.id
@@ -72,6 +82,7 @@ class ProductView(LoginRequiredMixin, TemplateView):
             fav_id = None
             isFav = False
 
+        # Контекстные значения для шаблона
         context['limit'] = limit
         context['list_recomend'] = recomend_list_products
         context['isFav'] = isFav
@@ -83,11 +94,13 @@ class ProductView(LoginRequiredMixin, TemplateView):
 
 
 class ProductsSearchView(LoginRequiredMixin, TemplateView):
+    ''' Поиск продукта '''
     template_name = 'shop_nike/shop.html'
     login_url = '/auth'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Формирование строки для LIKE запроса в SQL
         search = '.*' + self.request.GET.get('search', '') + '.*'
 
         products = Product.objects.filter(title_product__iregex=search)
