ec41c7bd4ec0ffa8f3722f41aa0443fc35bc2312
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-07-30 15:19:23 +0600

    rename model bucket and add on admin panel

--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,7 @@ from auth_nike.models import NikeUser
 from shop_nike.models import Product
 
 
-class BucketModel(models.Model):
+class Bucket(models.Model):
     user_id = models.ForeignKey(NikeUser, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
