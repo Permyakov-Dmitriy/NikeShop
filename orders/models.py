32eeecaa2b401b93a3d91c5b8e81c62aa1f245c0
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-07-31 20:46:07 +0600

    add column quantity for Basket

--- orders/models.py
+++ orders/models.py
@@ -8,6 +8,7 @@ from shop_nike.models import Product
 class Basket(models.Model):
     user_id = models.ForeignKey(NikeUser, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
     def __str__(self):
         return f'{self.user_id.email} {self.product_id.title_product}'
