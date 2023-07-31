50fc91b2fa6f41881c8f30a208e21a0a6246fe74
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-07-31 23:30:49 +0600

    add test for shop

--- shop_nike/tests.py
+++ shop_nike/tests.py
@@ -4,9 +4,12 @@ class TestShop(TestCase):
     def setUp(self):
         self.c = Client()
 
-    def test_response_status_code(self):
+    def test_response_status_code_for_gender(self):
         gender = ['men', 'women', 'baby']
-        
+
         for g in gender:
-            self.assertEqual(self.c.get(f'/shop/?gender={g}').status_code, 200)
+            self.assertEqual(self.c.get(f'/shop/?gender={g}').status_code, 302)
+
+    def test_response_status_code_for_gender(self):
+        self.assertEqual(self.c.get('/shop/find/?search=test').status_code, 302)
 
