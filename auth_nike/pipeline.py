bb85de039bcb07327f25ca501d518d5c147f2647
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-08-05 23:44:07 +0600

    TODO fix social core pipline

--- auth_nike/pipeline.py
+++ auth_nike/pipeline.py
@@ -5,17 +5,20 @@ from django.shortcuts import redirect, render
 @partial
 def send_email(backend, user, request, details, is_new=False, *args, **kwargs):
     e = request.session.get('email', '')
+    print(user)
+    print(details)
+    print(e)
     
     if is_new and not details['email']:
         if not e:
-            print(0)
             return redirect('VKEmail')
         
         else:
+            # TODO fix
             details['email'] = e
-            user.email = e
+            # user.email = e
 
-            try:
-                user.save()
-            except Exception:
-                return render(request, 'auth_nike/VKEmail.html', {'err': 'Почта уже существует'})
+            # try:
+            #     user.save()
+            # except Exception:
+            #     return render(request, 'auth_nike/VKEmail.html', {'err': 'Почта уже существует'})
